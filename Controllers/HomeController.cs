915a7a2b09034c65fc84f972c7d8526e35c8119e
Formation <Formation@HNI-DN-VM2023>
2024-09-24 11:11:22 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //methode "naturally" call by router
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //retourn to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "OpinionList":
+                        //TODO : code reading of the xml files provide
+                        OpinionList opinionList = new OpinionList();
+                        return View(id, opinionList.GetAvis("XlmFile/DataAvis.xml"));
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+                    default:
+                        //retourn to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //methode to send datas from form to validation page
+        [HttpPost]
+        public ActionResult ValidationFormulaire(/*[Bind("Id,Nom,Prenom,Sexe,Addresse,CodePostal,Ville,Email,DateFormation,Formation,AvisCobol,AvisCsharp")] */Avis avis)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return View("Form", avis);
+            }
+            return View(avis);
+
+
+        }
+    }
+}
---
+++ Models/Avis.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class Avis
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        public string? Nom { get; set; }
+
+
+        [Required]
+        public string? Prenom { get; set; }
+
+        [Required]
+        public string? Sexe { get; set; }
+
+        [Required]
+        public string? Adresse { get; set; }
+
+        [RegularExpression(@"\d{2}[ ]?\d{3}")]
+        [Display(Name = "Code postal")]
+        [Required]
+        public int? CodePostal { get; set; }
+
+
+        [Required]
+        public string? Ville { get; set; }
+
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Adresse email")]
+        [Required]
+        public string? Email { get; set; }
+
+        [Range(typeof(DateTime), "1/1/1966", "31/12/2020")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date début formation")]
+        [Required]
+        public DateTime DateFormation { get; set; }
+
+        [Display(Name = "Formation suivie")]
+        [Required]
+        public string? Formation { get; set; }
+
+        [Display(Name = "Formation cobol")]
+        [Required]
+        public string? AvisCobol{ get; set; }
+
+        [Display(Name = "Formation c#")]
+        [Required]
+        public string? AvisCsharp { get; set; }
+
+    }
+}
